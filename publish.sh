9f3615ed7f8b660f8bd67d5030584c462be261f1
Gianluca Ciccarelli <sturmer@gergel.im>
2021-06-08 21:10:01 +0300

    Add source for `Incartare`

--- publish.sh
+++ publish.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# Just publish the damn site
-set -e
-
-rm -rf output
-git clone . output
-
-cd output
-git checkout --orphan gh-pages
-git rm -rf .
-git commit --m 'Create gh-pages branch' --allow-empty
-
-git remote rm origin
-git remote add origin git@github.com:sturmer/sturmer.github.io.git
-
-bundle exec nanoc
-git push --delete origin gh-pages
-bundle exec nanoc deploy
+# To publish, just generate the site and git add the diff generated in the output
+# directory (which is a git repo with gh-pages branch).
